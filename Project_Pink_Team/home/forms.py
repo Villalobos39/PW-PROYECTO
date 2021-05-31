bcd0c0e450daf12f4e724b30e67e8bea9b06a003
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2021-05-31 13:26:43 -0700

    valio

---
+++ Project_Pink_Team/home/forms.py
@@ -0,0 +1,141 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UsernameField  
+from django.contrib.auth import get_user_model
+
+
+from .models import User, Usuario , Materia_Actual, Historial_Materias, Escuela, Grupo, Periodo
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+            model = User
+            fields = ("username",)
+            fields_classes = {"username": UsernameField}
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
+        exclude = 'id',
+
+
+
+##grupo
+
+class UpdateGrupoForm(forms.ModelForm):
+    class Meta:
+        model = Grupo
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateGrupoForm(forms.ModelForm):
+    class Meta:
+        model = Grupo
+        fields = "__all__"
+        exclude = 'id',
+
+##Usuario
+
+class UpdateUsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateUsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+        exclude = 'id',
+
+
+##Escuela
+
+class UpdateEscuelaForm(forms.ModelForm):
+    class Meta:
+        model = Escuela
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateEscuelaForm(forms.ModelForm):
+    class Meta:
+        model = Escuela
+        fields = "__all__"
+        exclude = 'id',
+
+##MateriaActual
+
+class UpdateMateria_ActualForm(forms.ModelForm):
+    class Meta:
+        model = Materia_Actual
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateMateria_ActualForm(forms.ModelForm):
+    class Meta:
+        model = Materia_Actual
+        fields = "__all__"
+        exclude = 'id',
+
+
+##Periodo
+
+class UpdatePeriodoForm(forms.ModelForm):
+    class Meta:
+        model = Periodo
+        fields = "__all__"
+        exclude = 'id',
+
+class CreatePeriodoForm(forms.ModelForm):
+    class Meta:
+        model = Periodo
+        fields = "__all__"
+        exclude = 'id',
+
+
+##Historial_Materias
+
+class UpdateHistorial_MateriasForm(forms.ModelForm):
+    class Meta:
+        model = Historial_Materias
+        fields = "__all__"
+        exclude = 'id',
+
+class CreateHistorial_MateriasForm(forms.ModelForm):
+    class Meta:
+        model = Historial_Materias
+        fields = "__all__"
+        exclude = 'id',
+
+#DOCENTE
+#Modificar materia alumno
+class Update_Calificacion(forms.ModelForm):
+    class Meta:
+        model = Materia_Actual
+        fields = "__all__"
+        exclude = 'id', 'alumno', 'nombre_materia', 'horario', 
+
+
+class RegistroForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = {          
+            'username',  
+            'first_name',
+            'last_name',
+            'email',            
+        }
+
+        labels = {
+            'username': "Nombre de Usuario",
+            'first_name': 'Nombre',
+            'last_name': 'Apellidos',
+            'email': 'Correo',
+        }
--- Project_Pink_Team/home/models.py
+++ Project_Pink_Team/home/models.py
@@ -1,3 +1,212 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Permission
+from django.contrib.auth.models import User
+from datetime import datetime    
+from django.utils.translation import ugettext as _
+#buscar como pasar multiples campos en def_str
+#nos aseguramos que los nombres de usuario sea facil identificar que rol tienen
+class User(AbstractUser):
+    pass
 
-# Create your models here.
+class Grupo(models.Model):
+    nombre_grupo=models.CharField(max_length=50)
+    turno=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_grupo
+
+class Usuario(models.Model):
+    MS_GRADOS = (
+        ("Primer grado", "Primer grado"),
+        ("Segundo grado","Segundo grado"),
+        ("Tercer grado","Tercer grado"),
+        ("Cuarto grado","Cuarto grado"),
+        ("Quinto grado","Quinto grado"),
+        ("Sexto grado","Sexto grado"),
+        ("Pendiente","Pendiente"),
+    )
+
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono_casa = models.CharField(max_length=32, default="pendiente")
+    telefono_cel = models.CharField(max_length=32, default="pendiente")
+    titulo_docente = models.CharField(max_length=50, default="pendiente")
+    grado_alumno = models.CharField(max_length=20, choices = MS_GRADOS, default="Pendiente")
+    curp = models.CharField(max_length=20, default="pendiente")
+    direccion = models.CharField(max_length=50, default="pendiente")
+    fecha_nacimiento = models.DateTimeField(default=datetime.now, blank=True)
+    register_timestamp = models.DateTimeField(auto_now_add=True)
+    register_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+# Se agregan permisos a la clase Usuario
+    class Meta:
+        permissions = (
+            ('is_teacher', _('Is Teacher')),
+            ('is_student', _('Is Student')),
+            ('is_admin', _('Is Admin')),
+        )
+
+class Escuela(models.Model):
+    nombre_institucion=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    nivel_educativo=models.CharField(max_length=20)
+    control=models.CharField(max_length=30)
+    turno=models.CharField(max_length=10)
+    clave=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre_institucion
+
+class Materia_Actual(models.Model):
+    MS_MATERIAS = (
+        #grado 1
+        ("Español 1", "Español 1"),
+        ("Matemáticas 1","Matemáticas 1"),
+        ("Exploración de la Naturaleza y la Sociedad 1","Exploración de la Naturaleza y la Sociedad 1"),
+        ("Formación Cívica y Ética 1","Formación Cívica y Ética 1"),
+        #grado 2
+        ("Español 2", "Español 2"),
+        ("Matemáticas 2","Matemáticas 2"),
+        ("Exploración de la Naturaleza y la Sociedad 2","Exploración de la Naturaleza y la Sociedad 2"),
+        ("Formación Cívica y Ética 2","Formación Cívica y Ética 2"),
+        #grado 3
+        ("Español 3", "Español 3"),
+        ("Matemáticas 3","Matemáticas 3"),
+        ("Ciencias Naturales 1","Ciencias Naturales 1"),
+        ("La Entidad donde vivo","La Entidad donde vivo"),
+        ("Formación Cívica y Ética 3","Formación Cívica y Ética 3"),
+        #grado 4
+        ("Español 4", "Español 4"),
+        ("Matemáticas 4","Matemáticas 4"),
+        ("Ciencias Naturales 2","Ciencias Naturales 2"),
+        ("Geografía 1","Geografía 1"),
+        ("Historia 1","Historia 1"),
+        ("Formación Cívica y Ética 4","Formación Cívica y Ética 4"),
+        #grado 5
+        ("Español 5", "Español 5"),
+        ("Matemáticas 5","Matemáticas 5"),
+        ("Ciencias Naturales 3","Ciencias Naturales 3"),
+        ("Geografía 2","Geografía 2"),
+        ("Historia 2","Historia 2"),
+        ("Formación Cívica y Ética 5","Formación Cívica y Ética 5"),
+        #grado 6
+        ("Español 6", "Español 6"),
+        ("Matemáticas 6","Matemáticas 6"),
+        ("Ciencias Naturales 4","Ciencias Naturales 4"),
+        ("Geografía 3","Geografía 3"),
+        ("Historia 3","Historia 3"),
+        ("Formación Cívica y Ética 6","Formación Cívica y Ética 6"),
+        ("Pendiente","Pendiente"),
+    )
+    MS_HORARIOS = (
+        #matutinos
+        ("8:00-9:00", "8:00-9:00"),
+        ("9:00-10:00", "9:00-10:00"),
+        ("10:00-11:00", "10:00-11:00"),
+        ("11:30-12:30", "11:30-12:30"),
+        ("12:30-13:30", "12:30-13:30"),
+        ("13:30-14:30", "13:30-14:30"),
+        #vespertinos
+        ("13:30-14:00", "13:30-14:00"),
+        ("14:00-15:00", "14:00-15:00"),
+        ("15:00-16:00", "15:00-16:00"),
+        ("16:30-17:00", "16:00-17:00"),
+        ("17:00-18:30", "17:30-18:30"),
+        ("18:30-19:30", "17:30-18:30"),
+        ("Pendiente","Pendiente"),
+    )
+    alumno = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre_materia=models.CharField(max_length=50, choices = MS_MATERIAS, default="Pendiente")
+    horario=models.CharField(max_length=50, choices = MS_HORARIOS, default="Pendiente")
+    b1=models.FloatField(default=0.0)
+    b2=models.FloatField(default=0.0)
+    b3=models.FloatField(default=0.0)
+    b4=models.FloatField(default=0.0)
+    b5=models.FloatField(default=0.0)
+    promedio=models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.nombre_materia
+
+class Periodo(models.Model):
+    nombre_periodo=models.CharField(max_length=40)
+    inicio_periodo=models.CharField(max_length=30)
+    fin_periodo=models.CharField(max_length=30)
+   
+    def __str__(self):
+        return self.nombre_periodo
+
+class Historial_Materias(models.Model):
+    MS_MATERIAS = (
+        #grado 1
+        ("Español 1", "Español 1"),
+        ("Matemáticas 1","Matemáticas 1"),
+        ("Exploración de la Naturaleza y la Sociedad 1","Exploración de la Naturaleza y la Sociedad 1"),
+        ("Formación Cívica y Ética 1","Formación Cívica y Ética 1"),
+        #grado 2
+        ("Español 2", "Español 2"),
+        ("Matemáticas 2","Matemáticas 2"),
+        ("Exploración de la Naturaleza y la Sociedad 2","Exploración de la Naturaleza y la Sociedad 2"),
+        ("Formación Cívica y Ética 2","Formación Cívica y Ética 2"),
+        #grado 3
+        ("Español 3", "Español 3"),
+        ("Matemáticas 3","Matemáticas 3"),
+        ("Ciencias Naturales 1","Ciencias Naturales 1"),
+        ("La Entidad donde vivo","La Entidad donde vivo"),
+        ("Formación Cívica y Ética 3","Formación Cívica y Ética 3"),
+        #grado 4
+        ("Español 4", "Español 4"),
+        ("Matemáticas 4","Matemáticas 4"),
+        ("Ciencias Naturales 2","Ciencias Naturales 2"),
+        ("Geografía 1","Geografía 1"),
+        ("Historia 1","Historia 1"),
+        ("Formación Cívica y Ética 4","Formación Cívica y Ética 4"),
+        #grado 5
+        ("Español 5", "Español 5"),
+        ("Matemáticas 5","Matemáticas 5"),
+        ("Ciencias Naturales 3","Ciencias Naturales 3"),
+        ("Geografía 2","Geografía 2"),
+        ("Historia 2","Historia 2"),
+        ("Formación Cívica y Ética 5","Formación Cívica y Ética 5"),
+        #grado 6
+        ("Español 6", "Español 6"),
+        ("Matemáticas 6","Matemáticas 6"),
+        ("Ciencias Naturales 4","Ciencias Naturales 4"),
+        ("Geografía 3","Geografía 3"),
+        ("Historia 3","Historia 3"),
+        ("Formación Cívica y Ética 6","Formación Cívica y Ética 6"),
+        ("Pendiente","Pendiente"),
+    )
+    MS_HORARIOS = (
+        #matutinos
+        ("8:00-9:00", "8:00-9:00"),
+        ("9:00-10:00", "9:00-10:00"),
+        ("10:00-11:00", "10:00-11:00"),
+        ("11:30-12:30", "11:30-12:30"),
+        ("12:30-13:30", "12:30-13:30"),
+        ("13:30-14:30", "13:30-14:30"),
+        #vespertinos
+        ("13:30-14:00", "13:30-14:00"),
+        ("14:00-15:00", "14:00-15:00"),
+        ("15:00-16:00", "15:00-16:00"),
+        ("16:30-17:00", "16:00-17:00"),
+        ("17:00-18:30", "17:30-18:30"),
+        ("18:30-19:30", "17:30-18:30"),
+        ("Pendiente","Pendiente"),
+    )
+    alumno = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre_materia=models.CharField(max_length=50, choices = MS_MATERIAS, default="Pendiente")
+    horario=models.CharField(max_length=50, choices = MS_HORARIOS, default="Pendiente")
+    horario=models.CharField(max_length=50)
+    b1=models.FloatField(default=0.0)
+    b2=models.FloatField(default=0.0)
+    b3=models.FloatField(default=0.0)
+    b4=models.FloatField(default=0.0)
+    b5=models.FloatField(default=0.0)
+    promedio=models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.nombre_materia
